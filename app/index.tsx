645c4730af89373986dcf9a36c3922ee3d70c8cd
scross52 <scross52@ivytech.edu>
2025-11-13 18:44:05 -0500

    Add todo basics

---
+++ app/index.tsx
@@ -0,0 +1,35 @@
+import { StyleSheet, Text, View } from "react-native";
+import Task from "../components/Task";
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.tasksWrapper}>
+      <Text style={styles.sectionTitle}>Today's Tasks</Text>
+
+      <View style={styles.items}>
+        <Task text={'Task 1'} />
+        <Task text={'Task 2'} />
+      </View>
+
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#E8EAED'
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  items: {
+    marginTop: 30,
+  }
+})
